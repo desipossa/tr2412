33122c8c3b0f74e6671fead1d8408b7776d4f097
desipossa <c1jang@hanmail.net>
2024-12-17 14:29:47 +0900

    20241217

--- hera/js/custom.js
+++ hera/js/custom.js
@@ -1,11 +1,31 @@
 $(function () {
 
+    const MAINVISUALSLIDE = new Swiper('.main_visual_slide', {
+        loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        effect: "creative",
+        creativeEffect: {
+            prev: {
+                shadow: true,
+                translate: ["-20%", 0, -1],
+            },
+            next: {
+                translate: ["100%", 0, 0],
+            },
+        },
 
-    $('.main_visual_slide').slick({
-        dots: true,
-        //fade: true,
-        //vertical: true,
-    });
+        navigation: {
+            nextEl: '.main_visual .arrows .next',
+            prevEl: '.main_visual .arrows .prev',
+        },
+
+    })
+
+
 
 
 })

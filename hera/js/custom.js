3f6a51464e803a379f3e8a28d1e1e690d5e9c5e0
desipossa <c1jang@hanmail.net>
2024-12-18 15:05:04 +0900

    20241218

--- hera/js/custom.js
+++ hera/js/custom.js
@@ -25,7 +25,27 @@ $(function () {
 
     })
 
+});
+
+$(function () {
+    $('.main_content .best_tab_menu button').on('click', function () {
+        let idx = $(this).parent().index();
+        $('.main_content .best_tab_menu button').removeClass('on');
+        $(this).addClass('on');
+        $('.main_content .best_tab_content .con').removeClass('on');
+        $('.main_content .best_tab_content .con').eq(idx).addClass('on')
+    })
+});
 
 
 
+$(function () {
+    $('.main_product .menu_wrap a').on('mouseenter', function () {
+        let idx = $(this).parent().index();
+        $('.main_product .menu_wrap a').removeClass('on');
+        $(this).addClass('on');
+        $('.main_product').css({
+            backgroundImage: `url(./images/main_product0${idx + 1}.jpg)`
+        })
+    })
 })
